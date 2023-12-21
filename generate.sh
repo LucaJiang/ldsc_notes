53c1fc2d29a26aa7835cf94c6d82a497291ee599
Jiang Wenxin <jiangwx7@qq.com>
2023-12-21 14:20:50 +0800

    Remove reveal.js configuration from generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,3 @@
 python3 md2nb.py index.md
-jupyter nbconvert index.ipynb --to slides --reveal-prefix reveal.js
---SlidesExporter.reveal_theme=black 
---SlidesExporter.reveal_scroll=True 
---SlidesExporter.reveal_transition=none
+jupyter nbconvert index.ipynb --to slides
 mv index.slides.html index.html
